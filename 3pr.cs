41cf8888f7b57e2542befff0df08db80ee596136
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2023-09-22 04:33:01 +0400

    Update 3pr.cs

--- 3pr.cs
+++ 3pr.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main()
     {
-        for(int i = 100; i <= 999; i++)
+        for (int i = 100; i <= 999; i++)
         {
-            if(i/100 == i/10%10 || i/100 == i%10 || i/10%10 == i%10 || i/100 == i/10%10 && i/100 == i%10)
+            if (i / 100 == i / 10 % 10 || i / 100 == i % 10 || i / 10 % 10 == i % 10 || i / 100 == i / 10 % 10 && i / 100 == i % 10)
             {
                 Console.WriteLine(i);
             }
