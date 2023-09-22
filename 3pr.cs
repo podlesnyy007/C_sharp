96a2a17757c0063592fd8fb16fade649236050e3
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2023-09-22 04:35:09 +0400

    Update 3pr.cs

--- 3pr.cs
+++ 3pr.cs
@@ -5,11 +5,7 @@ class Program
     static void Main()
     {
         for (int i = 100; i <= 999; i++)
-        {
             if (i / 100 == i / 10 % 10 || i / 100 == i % 10 || i / 10 % 10 == i % 10 || i / 100 == i / 10 % 10 && i / 100 == i % 10)
-            {
                 Console.WriteLine(i);
-            }
-        }
     }
 }
