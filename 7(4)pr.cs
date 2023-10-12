a83a43913a5dd56d3c40d5dbd9695a4f14be3eb7
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2023-10-12 12:51:02 +0400

    Update 7(4)pr.cs

--- 7(4)pr.cs
+++ 7(4)pr.cs
@@ -6,41 +6,55 @@ class Program
     {
         Console.Write("n = ");
         int n = int.Parse(Console.ReadLine());
-        Console.Write("m = ");
-        int m = int.Parse(Console.ReadLine());
         Console.WriteLine("Первый массив:");
-        int[][] mas = new int[n][];
-        for (int i = 0; i < mas.Length; i++)
+        int[][] mas_1 = new int[n][];
+        for (int i = 0; i < mas_1.Length; i++)
         {
-            mas[i] = new int[m];
-            for (int j = 0; j < mas[i].Length; j++)
+            mas_1[i] = new int[n];
+            for (int j = 0; j < mas_1[i].Length; j++)
             {
-                mas[i][j] = int.Parse(Console.ReadLine());
+                mas_1[i][j] = int.Parse(Console.ReadLine());
             }
         }
-        int[] b = new int[n];
-        for (int i = 0; i < n; i++)
+        int[] mas_2 = new int[n];
+        for (int i = 0; i < mas_2.Length; i++)
         {
-            b[i] = 1;
-            for (int j = 0; j < m; j++)
+            mas_2[i] = 1;
+            for (int j = 0; j < mas_1[i].Length; j++)
             {
-                b[i] *= mas[j][i];
+                mas_2[i] *= mas_1[j][i];
             }
         }
-        Console.WriteLine("Второй массив");
-        foreach (int elem in b)
+        Console.WriteLine("Второй массив:");
+        foreach (int elem in mas_2)
         {
             Console.Write(elem + " ");
         }
-
-        int min = b[0]; //в качестве наименьшего значения полагаем нулевой элемент массива
-        for (int i = 0; i < b.Length; i++)
+        int min = mas_2[0]; //в качестве наименьшего значения полагаем нулевой элемент массива
+        for (int i = 0; i < mas_2.Length; i++)
         {
-            if (b[i] < min)
+            if (mas_2[i] < min)
             {
-                min = b[i];
+                min = mas_2[i];
             }
         }
-        Console.WriteLine("Минимальный элеменет во втором массиве\n" + min);
+        Console.WriteLine("\nМинимальный элеменет во втором массиве: " + min);
     }
 }
+
+/*
+n = 3
+Первый массив:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+Второй массив:
+28 80 162
+Минимальный элеменет во втором массиве: 28
+*/
