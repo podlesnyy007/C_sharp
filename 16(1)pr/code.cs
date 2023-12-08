b668a4e3568e63e96b9b0655ffd88ba6009d1259
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2023-12-08 04:10:43 +0400

    Create code.cs

---
+++ 16(1)pr/code.cs
@@ -0,0 +1,49 @@
+/*
+Решить задачи из практикума №15 I, используя методы расширения
+Дана последовательность целых чисел.
+Замечание. Для хранения последовательности можно использовать одномерный массив 
+или любую другую подходящую коллекцию. Выбор структуры данных обосновать. Ввод вывод 
+данных – файловый.
+Вывести на экран в порядке убывания все отрицательные трехзначные числа, заменив 
+них на противоположное значение.
+*/
+
+using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        StreamReader fileIn = new StreamReader("d:/MetodRasshireniya1/input.txt");
+        List<int> list = new List<int>();
+        while (!fileIn.EndOfStream)
+        {
+            string[] str = fileIn.ReadLine().Split(new char[] { ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < str.Length; i++)
+            {
+                list.Add(int.Parse(str[i]));
+            }
+        }
+        fileIn.Close();
+        var numbers = list.Where(n => n >= -999 && n <= -100).Select(x => -x).OrderByDescending(n => n);
+        StreamWriter fileOut = new StreamWriter("d:/MetodRasshireniya1/output.txt");
+        foreach (int x in numbers)
+        {
+            fileOut.WriteLine("{0}", x);
+        }
+        fileOut.Close();
+    }
+}
+
+/*
+-123 45 -678 987 -456
+-50 999 -222 -101 0
+
+678
+456
+222
+123
+101
+*/
